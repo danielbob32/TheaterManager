d682ee2aaa3da5dd33f82dee2654174799b619e0
Doston1 <dt2001.di@gmail.com>
2024-07-06 16:24:22 +0300

    Solved some issues with the entities, make sure you update from this push

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/ServerDB.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/ServerDB.java
@@ -1,17 +1,20 @@
 package il.cshaifasweng.OCSFMediatorExample.server;
 
 import il.cshaifasweng.OCSFMediatorExample.entities.*;
-import java.util.List;
-import java.util.Random;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
-import java.util.Scanner;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 
 public class ServerDB {
     private SessionFactory sessionFactory;
@@ -19,39 +22,123 @@ public class ServerDB {
 
     public ServerDB() {
         try {
-
+            // Configure Hibernate
             Configuration configuration = new Configuration();
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter your password: ");
-            String password = scanner.nextLine();
-            configuration.setProperty("hibernate.connection.password",password);
-            configuration.addAnnotatedClass(Movie.class);
+            configuration.addProperties(getHibernateProperties());
 
+            // Add all entity classes here
+            configuration.addAnnotatedClass(Booking.class);
+            configuration.addAnnotatedClass(Cinema.class);
+            configuration.addAnnotatedClass(CinemaManager.class);
+            configuration.addAnnotatedClass(Complaint.class);
+            configuration.addAnnotatedClass(Customer.class);
+            configuration.addAnnotatedClass(HomeMovieLink.class);
+            configuration.addAnnotatedClass(Movie.class);
+            configuration.addAnnotatedClass(MovieHall.class);
+            configuration.addAnnotatedClass(Product.class);
+            configuration.addAnnotatedClass(Screening.class);
+            configuration.addAnnotatedClass(Seat.class);
+            configuration.addAnnotatedClass(Ticket.class);
+            configuration.addAnnotatedClass(TicketTab.class);
+            configuration.addAnnotatedClass(Warning.class);
+            configuration.addAnnotatedClass(Worker.class);
+
+            // Build the SessionFactory
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties()).build();
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             session = sessionFactory.openSession();
             System.out.println("Connected to database");
 
-
         } catch (HibernateException e) {
+            System.err.println("Error connecting to database: " + e.getMessage());
             e.printStackTrace();
-            // Handle exception appropriately, e.g., logging or throwing a runtime exception
+        }
+
+        addTestData();
+    }
+
+    private void addTestData() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            // Add a test worker
+            Worker testWorker = new Worker("Test Worker", "password123", 1555);
+            session.save(testWorker);
+
+            // Add a test customer
+            Customer testCustomer = new Customer("Test Customer", "test@example.com", 1545);
+            session.save(testCustomer);
+
+            transaction.commit();
+            System.out.println("Test data added successfully");
+        } catch (Exception e) {
+            System.err.println("Error adding test data: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            System.err.println("Can't find hibernate.properties file");
+            e.printStackTrace();
+        }
+        return properties;
+    }
 
     public List<Movie> getAllMovies() throws Exception {
-//        System.out.println("In ServerDB getAlMovies method");
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<Movie> query = builder.createQuery(Movie.class);
         query.from(Movie.class);
         List<Movie> data = session.createQuery(query).getResultList();
-//        System.out.println("In ServerDB returning the movie list");
         return data;
     }
 
+    public boolean checkWorkerCredentials(int id, String password) {
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "FROM Worker W WHERE W.id = :id";
+            Query<Worker> query = session.createQuery(hql, Worker.class);
+            query.setParameter("id", id);
+            Worker worker = query.uniqueResult();
 
+            System.out.println("Checking worker credentials for ID: " + id);
+            System.out.println("Found worker: " + (worker != null));
+            if (worker != null) {
+                System.out.println("Password match: " + worker.getPassword().equals(password));
+            }
+
+            return worker != null && worker.getPassword().equals(password);
+        } catch (Exception e) {
+            System.err.println("Error checking worker credentials: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean checkCustomerCredentials(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "FROM Customer C WHERE C.id = :id";
+            Query<Customer> query = session.createQuery(hql, Customer.class);
+            query.setParameter("id", id);
+            Customer customer = query.uniqueResult();
+
+            System.out.println("Checking customer credentials for ID: " + id);
+            System.out.println("Found customer: " + (customer != null));
+
+            return customer != null;
+        } catch (Exception e) {
+            System.err.println("Error checking customer credentials: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     // Close the session factory and session when the instance is no longer needed
     public void close() {
@@ -62,6 +149,4 @@ public class ServerDB {
             sessionFactory.close();
         }
     }
-
-
 }
