df8185759e34d8d773a5b5e187c9eee0546f8c02
yarden_itzhaky <yardene015@gmail.com>
2024-08-13 15:32:03 +0300

    APP - changed the first screen that popsup customer - added implements Serializable (needed for תקשורת בינהם), add a needed constructor worker - same as customer added a loginpage controller and fxml files ServerDB - added check methods for the login attempt simple client - added handle for login attempt simple server - added handle login

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/CustomerMenuController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/CustomerMenuController.java
@@ -6,33 +6,37 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import java.io.IOException;
 
-public class CustomerMenuController implements DataInitializable{
+public class CustomerMenuController implements DataInitializable {
 
     private SimpleClient client;
 
     @FXML
     private Label welcomeLabel;
 
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private VBox buttonBox;
+
     @Override
     public void setClient(SimpleClient client) {
         this.client = client;
     }
-//MESSAGE FOR THE NEW MOVIE ADD
-//    @FXML
-//    public void initialize() {
-//        EventBus.getDefault().register(this);
-//        // ... other initialization code
-//    }
 
     @Override
     public void initData(Object data) {
         System.out.println("CustomerMenuController initialized");
         updateWelcomeMessage();
+        addDynamicEffects();
     }
 
     private void updateWelcomeMessage() {
@@ -42,10 +46,30 @@ public class CustomerMenuController implements DataInitializable{
         }
     }
 
+    private void addDynamicEffects() {
+        // Adding a particle effect
+        for (int i = 0; i < 100; i++) {
+            javafx.scene.shape.Circle particle = new javafx.scene.shape.Circle(2, javafx.scene.paint.Color.web("rgba(255, 255, 255, 0.8)"));
+            rootPane.getChildren().add(particle);
+
+            double startX = Math.random() * rootPane.getWidth();
+            double startY = Math.random() * rootPane.getHeight();
+
+            particle.setTranslateX(startX);
+            particle.setTranslateY(startY);
+
+            javafx.animation.FadeTransition fadeTransition = new javafx.animation.FadeTransition(Duration.seconds(4), particle);
+            fadeTransition.setFromValue(1);
+            fadeTransition.setToValue(0);
+            fadeTransition.setCycleCount(javafx.animation.FadeTransition.INDEFINITE);
+            fadeTransition.setAutoReverse(true);
+            fadeTransition.play();
+        }
+    }
+
     @FXML
     private void viewFutureMovies() throws IOException {
         Person connectedPerson = client.getConnectedPerson();
-
         App.setRoot("FutureMoviesPage", connectedPerson);
     }
 
@@ -59,7 +83,6 @@ public class CustomerMenuController implements DataInitializable{
     @FXML
     private void viewMovieList() throws IOException {
         Person connectedPerson = client.getConnectedPerson();
-
         App.setRoot("CinemaMovieList", connectedPerson);
     }
 
@@ -76,31 +99,15 @@ public class CustomerMenuController implements DataInitializable{
         }
     }
 
-//    @FXML
-//    private void buyTickets() throws IOException {
-//        Person connectedPerson = client.getConnectedPerson();
-//
-//        App.setRoot("BuyTickets", connectedPerson);
-//    }
-
-//    @FXML
-//    private void buyHomeTickets() throws IOException {
-//        Person connectedPerson = client.getConnectedPerson();
-//
-//        App.setRoot("BuyHomeTickets", connectedPerson);
-//    }
-
     @FXML
     private void buyTicketTab() throws IOException {
         Person connectedPerson = client.getConnectedPerson();
-
         App.setRoot("PurchaseTicketTab", connectedPerson);
     }
 
     @FXML
     private void refundTickets() throws IOException {
         Person connectedPerson = client.getConnectedPerson();
-
         App.setRoot("RefundTickets", connectedPerson);
     }
 
